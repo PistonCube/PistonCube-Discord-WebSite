77a7860961767e138f515d14f64e48b9c868412b
PistonCube <93083195+PistonCube@users.noreply.github.com>
2026-03-09 13:15:43 -0300

    Update discordservers_es.js

--- assets/js/discordservers_es.js
+++ assets/js/discordservers_es.js
@@ -2,11 +2,10 @@ const servers = [
     { inviteCode: '283JBXufXm', title: 'PistonProjects Support', introText: 'Mi servidor donde publico mis proyectos:' },
     { inviteCode: 'eKeeZPq6SJ', title: 'PistonCubeBOT Support', introText: 'Soporte para mi bot de Minecraft:' },
     { inviteCode: 'XZD5W4Zadc', title: '2b2t Argentina Server', introText: 'Discord oficial de 2b2t Argentina:' },
+    { inviteCode: 'wfqSRxeStV', title: '2b2t Guild Public', introText: 'Guild de 2b2t con recursos, guías y comunidad:' },
     { inviteCode: 'xqQCHk5JwU', title: 'ZuruBOT', introText: 'Únete al Discord de soporte de ZuruBOT:' },
-    { inviteCode: '5sgp2TW5mp', title: 'HidenCloud Server', introText: 'Únete al Discord de soporte de HidenCloud:' },
     { inviteCode: 'MaXTXuXA6s', title: 'ResistenciaBananera', introText: 'Únete al Discord de ResistenciaBananera:' },
     { inviteCode: 'nbYFdHXjFw', title: 'CubeStone Studios', introText: 'Únete al Discord oficial de CubeStone Studios:' },
-    { inviteCode: '3un38K8ZJd', title: 'GamersLatinos', introText: 'Únete al Discord oficial de GamerLatinos:' },
     { inviteCode: 'T68FzkYDAg', title: 'Astros Ads', introText: 'Únete a mi servidor de publicidad:' }
 ];
 
